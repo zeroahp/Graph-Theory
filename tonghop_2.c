b3b5c51e3b8784ca095fedfd745ce31e572f1a35
ahpzero <121105267+zeroahp@users.noreply.github.com>
2023-01-08 16:34:58 +0700

    Create tonghop_2.c

---
+++ tonghop_2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define MAX_M 50
+
+typedef struct{
+    int u, v;
+}Edge;
+
+typedef struct 
+{
+    int n,m;
+    Edge edges[MAX_M];
+}Graph;
+
+void init_graph(Graph *pG, int n){
+    pG->n = n;
+    pG->m = 0;
+}
+
+void add_edge(Graph *pG, int u, int v){
+    pG->edges[pG->m].u = u;
+    pG->edges[pG->m].v = v;
+    pG->m ++;
+}
+
+int degree(Graph *pG, int u){
+    int i, cnt = 0;
+    for(i =0 ;i<pG->m ;i++){
+        if(pG->edges[i].u == u)
+            cnt++;
+        if(pG->edges[i].v == u)
+            cnt++;
+    }
+    return cnt;
+}
+
+int main(){
+    Graph G;
+    int n, m;
+    printf("Nhap va so dinh va so cung cua do thi :");
+    scanf("%d%d",&n,&m);
+    G.n = n;
+    init_graph(&G,n);
+    int i,u,v;
+    for( i = 0; i< m ; i++){
+        printf("Nhap cung u va v :");
+        scanf("%d %d", &u, &v);
+        add_edge(&G,u,v);
+    }
+    for( i = 1 ; i<= n ; i++){
+        printf("deg(%d) = %d\n",i,degree(&G,i));
+    }
+    return 0;
+}
